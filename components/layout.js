df3491357c0cf8890e0cfacdefb4a9055f95319d
sracela <saracelalfonso@gmail.com>
2020-07-29 01:21:24 +0200

    Add navigation bar to layout. Style layout component

--- components/layout.js
+++ components/layout.js
@@ -16,23 +16,31 @@ export default function Layout({ children, home }) {
                 />
                 <meta name="og:title" content={siteTitle} />
             </Head>
-            <header className={styles.header}></header>
-            <main>{children}</main>
-            {!home && (
-                <div className={styles.backToHome}>
-                    <Link href="/">
-                        <a>← Back to home</a>
-                    </Link>
-                </div>            
-            )}
-            {!home && (
-                <div className={styles.backToPrevious}>
-                    <Link href="/">
-                        <a>Previous page</a>
-                    </Link>
-                </div>
-            )}
-            <footer>Powered by sracela</footer>
+            <header>
+                <nav className={styles.nav}>
+                    <Link href="/"><a className={styles.logo}>pix2code</a></Link>
+                    <img className={styles.hamburguer} src="/images/icons8-hamburguesa-50.png" alt="hamburger" />
+                </nav>
+            </header>
+            <main className={styles.main}>
+                {children}
+            </main>
+            <footer className={styles.footer}>
+                <p>Powered by sracela</p>
+                {!home && (
+                    <div className={styles.backToHome}>
+                        <Link href="/">
+                            <a>← Back to home</a>
+                        </Link>
+                    </div>            
+                )}
+                {!home && (
+                    <div className={styles.backToPrevious}>
+                        <Link href="/">
+                            <a>Previous page</a>
+                        </Link>
+                    </div>)}
+            </footer>
         </div>
     )
     
