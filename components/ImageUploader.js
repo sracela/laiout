140be85e3a4a1a5826c48944cddbcb9a11f6417f
sracela <saracelalfonso@gmail.com>
2020-07-29 19:39:41 +0200

    Logic and style ImageUploader

--- components/ImageUploader.js
+++ components/ImageUploader.js
@@ -1,5 +1,38 @@
-
+import Link from 'next/link'
+import styles from '../components/ImageUploader.module.css'
+import { useState, useRef } from 'react'
 
 export default function ImageUploader() {
- return <div></div>
+    const [image, setImage] = useState('');
+    const inputFileRef = useRef(null);
+    
+    const onImageChange = event => {
+        if (event.target.files && event.target.files[0]) {
+          let img = event.target.files[0];
+          setImage(URL.createObjectURL(img))
+        }
+      };
+
+ return (
+     <div className={styles.fileUpload}>
+        <button className={styles.fileUploadBtn} type="button" onClick={() => {inputFileRef.current.click();}}>Add Image</button>
+        <div className={styles.imageUploadWrap}>
+            <input className={styles.fileUploadInput} type='file' onChange={onImageChange} ref={inputFileRef} />
+            {image ? <div className={styles.fileUploadContent} >
+            <img className={styles.fileUploadImage} src={image} />
+         </div> :
+         <div className={styles.dragText}>
+                <h3>Drag and drop a file </h3>
+                <h3>or</h3> 
+                <h3>select add Image</h3>
+            </div>}
+
+         </div>
+         {image && <Link href="/compute_layout" >
+                <div className={styles.card}>
+                    Get your code!
+                </div>
+            </Link>}
+     </div>
+ )
 }
--- pages/upload_image.js
+++ pages/upload_image.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Link from 'next/link'
 import Layout, { siteTitle } from '../components/layout'
 import ImageUploader from '../components/ImageUploader'
 import styles from '../styles/Home.module.css'
@@ -10,14 +9,9 @@ export default function Upload_Image() {
         <Head>
           <title>{siteTitle}</title>
         </Head>
-        <section>
+        <section className={styles.grid}>
           <h1 className={styles.title}>Upload an image</h1>
-          <Link href="/compute_layout" >
-          <div className={styles.card}>
-              <a>Upload an image</a>
-          </div>
-        </Link>
-          {/* <ImageUploader /> */}
+          <ImageUploader />
         </section>
       </Layout>
     )
