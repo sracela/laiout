4f24859114a8c250a53861bf54aa0f851b16426f
sracela <saracelalfonso@gmail.com>
2020-07-30 12:49:20 +0200

    Add logic and style to compute_layout page

--- pages/compute_layout.js
+++ pages/compute_layout.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/ComputeLayout.module.css'
 import { FileUploadContent } from '../components/ImageUploader'
 import { useRouter } from 'next/router'
 
@@ -13,16 +13,19 @@ export default function ComputeLayout() {
         <title>{siteTitle}</title>
       </Head>
       <section className={styles.grid}>
-        <h1 className={styles.title}> Here is your code ! </h1>
-        <div className={styles.gridResults}>
-            <div className={styles.card}>
+        <h1 className={styles.title}>Here is your code !</h1>
+        <div className={styles.resultsContainer}>
+            <div className={styles.resultCard}>
+                Image
                 <FileUploadContent content={image}/> 
             </div>
-            <div className={styles.card}>
+            <div className={styles.resultCard}>
               DSL
+              <p className={styles.code}>ldjaskdjlaskdjlsa djksdjalkjd djksdlasjd</p>
             </div>
-            <div className={styles.card}>
+            <div className={styles.resultCard}>
              Real code
+              <p className={styles.code}>ldjaskdjlaskdjlsa djksdjalkjd djksdlasjd</p>
             </div>
         </div>
 
